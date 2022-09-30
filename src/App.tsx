cae9869b6c65208b2bb4695c9428c80dbbc7f1dd
Hamafac <borysenko777@gmail.com>
2022-09-30 21:43:48 +0300

    Add figures on the board

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
    function restart() {
       const newBoard = new Board();
       newBoard.initCells()
+      newBoard.addFigures()
       setBoard(newBoard)
    }
 
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -9,7 +9,7 @@ interface CellProps {
 const CellComponent: FC<CellProps> = ({ cell }) => {
    return (
       <div className={['cell', cell.color].join(' ')}>
-
+         {cell.figure?.logo && <img src={cell.figure.logo} alt='Figure' />}
       </div>
    );
 }
