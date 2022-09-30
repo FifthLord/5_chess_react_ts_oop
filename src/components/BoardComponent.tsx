72ac84afb5069d81eb4d5e18503947f14363cd90
Hamafac <borysenko777@gmail.com>
2022-09-30 22:06:32 +0300

    started add logic, add selected cell

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
 
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Board } from "../models/Board";
+import { Cell } from "../models/Cell";
 import CellComponent from "./CellComponent";
 
 
@@ -14,7 +15,7 @@ interface BoardProps {
 
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
-
+   const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
    return (
       <div className='board'>
          {board.cells.map((row, index) =>
@@ -23,6 +24,7 @@ const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
                   <CellComponent
                      cell={cell}
                      key={cell.id}
+                     selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
                   />
                )}
             </React.Fragment>
--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -3,12 +3,13 @@ import React, { FC } from 'react';
 import { Cell } from '../models/Cell';
 
 interface CellProps {
-   cell: Cell
+   cell: Cell;
+   selected: boolean;
 }
 
-const CellComponent: FC<CellProps> = ({ cell }) => {
+const CellComponent: FC<CellProps> = ({ cell, selected }) => {
    return (
-      <div className={['cell', cell.color].join(' ')}>
+      <div className={['cell', cell.color, selected ? "selected" : ''].join(' ')}>
          {cell.figure?.logo && <img src={cell.figure.logo} alt='Figure' />}
       </div>
    );
