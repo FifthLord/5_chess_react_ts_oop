25dd7416bdffa2edd0244d8e0535c21e25401dd9
Hamafac <borysenko777@gmail.com>
2022-10-01 16:55:26 +0300

    Add logic game - change color selected figure

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,5 +1,6 @@
 
 import React, { FC, useState } from "react";
+import { useEffect } from "react";
 import { Board } from "../models/Board";
 import { Cell } from "../models/Cell";
 import CellComponent from "./CellComponent";
@@ -16,12 +17,34 @@ interface BoardProps {
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
    const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+   function click(cell: Cell) {
+      if (cell.figure) {
+         setSelectedCell(cell);
+      }
+   };
+
+   useEffect(() => {
+      highLightCells()
+   }, [selectedCell])
+
+   function highLightCells() {
+      board.highLightCells(selectedCell)
+      updateBoard()
+   };
+
+   function updateBoard() {
+      const newBoard = board.getCopyBoard()
+      setBoard(newBoard)
+   }
+
    return (
       <div className='board'>
          {board.cells.map((row, index) =>
             <React.Fragment key={index}>
                {row.map(cell =>
                   <CellComponent
+                     click={click}
                      cell={cell}
                      key={cell.id}
                      selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
