d52538c9e417b38cae4abff9b0f4eb762c057860
Hamafac <borysenko777@gmail.com>
2022-10-07 17:52:18 +0300

    add Timer logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import BoardComponent from './components/BoardComponent';
 import LostFigures from './components/LostFigures';
+import Timer from './components/Timer';
 import { Board } from './models/Board';
 import { Colors } from './models/Colors';
 import { Player } from './models/Player';
@@ -34,6 +35,10 @@ const App = () => {
 
    return (
       <div className="app">
+         <Timer
+            restart={restart}
+            currentPlayer={currentPlayer}
+         />
          <BoardComponent
             board={board}
             setBoard={setBoard}
---
+++ src/components/Timer.tsx
@@ -0,0 +1,45 @@
+import React, { FC, useEffect, useRef, useState } from 'react';
+import { Colors } from '../models/Colors';
+import { Player } from '../models/Player';
+
+interface TimerProps {
+   currentPlayer: Player | null;
+   restart: () => void;
+}
+
+const Timer: FC<TimerProps> = ({ currentPlayer, restart }) => {
+   const [blackTime, setBlackTime] = useState(300);
+   const [whiteTime, setWhiteTime] = useState(300);
+   const timer = useRef<null | ReturnType<typeof setInterval>>(null)
+
+   useEffect(() => {
+      startTimer()
+   }, [currentPlayer])
+
+   function startTimer() {
+      if (timer.current) {
+         clearInterval(timer.current)
+      }
+      const callback = currentPlayer?.color === Colors.WHITE ? decrementWhiteTimer : decrementBlackTimer
+      timer.current = setInterval(callback, 1000)
+   }
+   function decrementBlackTimer() {
+      setBlackTime(prev => prev - 1)
+   }
+   function decrementWhiteTimer() {
+      setWhiteTime(prev => prev - 1)
+   }
+
+
+   return (
+      <div>
+         <div>
+            <button onClick={restart}>Restart game</button>
+         </div>
+         <h2>Чорні - {blackTime}</h2>
+         <h2>Білі - {whiteTime}</h2>
+      </div>
+   );
+};
+
+export default Timer;
